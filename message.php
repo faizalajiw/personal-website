17908a40925b47cd20700a7c71c21d7fe99c0297
Faizal Aji Wibowo <74334556+faizalaji7@users.noreply.github.com>
2022-01-06 23:06:00 +0700

    update php mailer

--- message.php
+++ message.php
@@ -17,7 +17,7 @@ $message = $_POST['message'];
 
 if (!empty($email) && !empty($message)) { //jika email dan message tidak diisi 
    if (filter_var($email, FILTER_VALIDATE_EMAIL)) { //jika user memasukkan email yg valid
-      $receiver = "faizalajiwibowo51@gmail.com"; //email penerima
+      $receiver = "faizalajiwibowo9@gmail.com"; //email penerima
       $subject = "From: $name <$email>"; //nanti bakal keliatan contohnya From: Faizal <blabla@gmail.com>
       $body = "Name: $name\nEmail: $email\nTopic: $topic\n\nMessage: $message\n\nRegards,\n$name";
       $sender = "From: $email"; //email pengirim
@@ -34,7 +34,7 @@ if (!empty($email) && !empty($message)) { //jika email dan message tidak diisi
          $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
          //Recipients
-         $mail->setFrom('faizalajiwibowo51@gmail.com', 'Agent Alfin');
+         $mail->setFrom('faizalajiwibowo51@gmail.com', 'Agent Faizal');
          $mail->addAddress($receiver);
          // $mail->addAddress('iyalfin@gmail.com');
 
